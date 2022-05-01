e4e1d344f87184f382c77715f876f17dba2d849a
PhilSasko <sasko@live.se>
2022-05-01 13:59:07 +0200

    Set up dependency injection

--- MyPoker.Wpf/App.xaml.cs
+++ MyPoker.Wpf/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyPoker.PokerLogic.HandCalculation;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +17,43 @@ namespace MyPoker.Wpf
     /// </summary>
     public partial class App : Application
     {
+        // followed tutorial at: https://marcominerva.wordpress.com/2019/11/07/update-on-using-hostbuilder-dependency-injection-and-service-provider-with-net-core-3-0-wpf-applications/
+        private readonly IHost host;
+
+        public App()
+        {
+            host = Host.CreateDefaultBuilder()
+                   .ConfigureServices((context, services) =>
+                   {
+                       ConfigureServices(context.Configuration, services);
+                   })
+                   .Build();
+        }
+
+        private void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddTransient<IPokerHandCalculator, PokerHandCalculator>();
+            services.AddSingleton<MainWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await host.StartAsync();
+
+            var mainWindow = host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (host)
+            {
+                await host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+
+            base.OnExit(e);
+        }
     }
 }
